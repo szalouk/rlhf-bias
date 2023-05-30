a404f0713990629bdca99029cd99ae072d01d864
SZALOUK1 <szalouk1@ford.com>
2023-05-29 21:14:18 -0700

    Moved supervised_finetuning script to top of repo. Modified the supervised_finetuning.sh script to include a batch_size parameter.

--- scripts/supervised_finetuning.sh
+++ scripts/supervised_finetuning.sh
@@ -2,11 +2,13 @@
 
 MODEL_PATH=$1
 
-torchrun --nnodes 1  --nproc_per_node 1 \
-	examples/stack_llama/scripts/supervised_finetuning.py \
+torchrun --nnodes 1  --nproc_per_node 4 \
+	supervised_finetuning.py \
 	--model_path=${MODEL_PATH} \
 	--streaming \
 	--no_gradient_checkpointing \
 	--learning_rate 1e-5 \
 	--max_steps 5000 \
+	--batch_size 4 \
+	--bf16 \
 	--output_dir ./supervised_finetuning_out
