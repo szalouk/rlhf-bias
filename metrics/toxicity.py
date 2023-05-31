44d0bd1dd96d860d69f33f1c817d4b72c790aab3
Sofian Zalouk <sofianzalouk@Sofians-MacBook-Pro.local>
2023-05-31 00:19:52 -0700

    Modified metrics to use ppo_trainer.generate method for multi-gpu training.

--- metrics/toxicity.py
+++ metrics/toxicity.py
@@ -9,7 +9,7 @@ def get_random_prompts(dataset, num_examples=100):
     return(dataset[picks])
 
 class ToxicityMetric:
-    def __init__(self, num_examples=5):
+    def __init__(self, num_examples=100):
         toxicity_prompts = load_dataset("allenai/real-toxicity-prompts", split="train")
         toxic_sample= get_random_prompts(toxicity_prompts, num_examples)
         self.toxic_prompts = [p['text'] for p in toxic_sample['prompt']]
@@ -25,9 +25,13 @@ class ToxicityMetric:
             inputs = tokenizer(prompt, return_tensors="pt")
             inputs = {k: v.to(device) for k, v in inputs.items()}
             outputs = ppo_trainer.generate(inputs['input_ids'].squeeze(0), return_prompt=False, max_length=50, do_sample=False, pad_token_id=50256)
+            all_outputs = ppo_trainer.accelerator.gather_for_metrics(outputs)
+            print(f'all_outputs.shape = {all_outputs.shape}')
             # outputs = model.generate(**inputs, labels=inputs["input_ids"], max_length=50, do_sample=False, pad_token_id=50256)
-            continuation = tokenizer.decode(outputs[0])
-            model_continuations.append(continuation)
+            continuation = tokenizer.batch_decode(all_outputs)
+            model_continuations.extend(continuation)
+            print(f'len(model_continuations) = {model_continuations}')
+            # model_continuations.append(continuation)
         
         toxicity_ratio = self.toxicity.compute(predictions=model_continuations, aggregation="ratio")
         max_toxicity = self.toxicity.compute(predictions=model_continuations, aggregation="maximum")
