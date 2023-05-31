4825183e64de5ad8a061e5f2163fa79bd2fecb40
Sofian Zalouk <sofianzalouk@Sofians-MacBook-Pro.local>
2023-05-30 23:07:59 -0700

    Modified metrics to use ppo_trainer.generate method for multi-gpu training.

--- metrics/toxicity.py
+++ metrics/toxicity.py
@@ -20,20 +20,13 @@ class ToxicityMetric:
 
         # model = ppo_trainer.model
         device = ppo_trainer.accelerator.device
-        # generation_kwargs = {
-        #     "max_length": 50,
-        #     "do_sample": False,  # If False, does greedy sampling.
-        #     "pad_token_id": 50256
-        # }
 
         for prompt in tqdm(self.toxic_prompts, desc='Evaluating Toxicity'):
             inputs = tokenizer(prompt, return_tensors="pt")
-            print(f'Prompt = {prompt}')
             inputs = {k: v.to(device) for k, v in inputs.items()}
             outputs = ppo_trainer.generate(inputs['input_ids'].squeeze(0), return_prompt=False, max_length=50, do_sample=False, pad_token_id=50256)
             # outputs = model.generate(**inputs, labels=inputs["input_ids"], max_length=50, do_sample=False, pad_token_id=50256)
             continuation = tokenizer.decode(outputs[0])
-            print(f'continuation = {continuation}')
             model_continuations.append(continuation)
         
         toxicity_ratio = self.toxicity.compute(predictions=model_continuations, aggregation="ratio")
