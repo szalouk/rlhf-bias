89e1b2fa654e8b43c06eeab6157d01b2ec582fcb
Sofian Zalouk <46830156+szalouk@users.noreply.github.com>
2023-06-12 22:09:28 -0700

    Update reward_modeling.sh

--- scripts/reward_modeling.sh
+++ scripts/reward_modeling.sh
@@ -3,7 +3,7 @@
 MODEL_NAME=$1
 
 torchrun --nnodes 1  --nproc_per_node 4 \
-	examples/stack_llama/scripts/reward_modeling.py \
+	reward_modeling.py \
 	--model_name=${MODEL_NAME} \
 	--bf16=True \
 	--per_device_train_batch_size=2 \
